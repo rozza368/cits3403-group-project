84917a9cb4537758da63d43597f143f2a73fb18b
Michaelallen5 <michaelallen2359@gmail.com>
2025-05-14 17:04:51 +0800

    Made the secret key now take from a .env file which is ignored in the new .gitignore

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__name__))
 default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
-    SECRET_KEY = 'temporary_password'
+    SECRET_KEY = os.getenv('SECRET_KEY')
