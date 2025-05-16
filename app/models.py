fc5571f7d37cbec09859af30339ba87037c6e071
Michaelallen5 <michaelallen2359@gmail.com>
2025-05-16 15:59:08 +0800

    Made email within User class unique and non-nullable.

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
-    email = db.Column(db.String(120), nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
 
     trades = db.relationship('Trade', backref='user', lazy=True)
